311fd29869b1e075cb15f182ee3d7497e9c8af1c
Kevin Smeraglio <ksmera01@gmail.com>
2020-05-26 09:12:00 -0400

    updated schema, seeds and connection.js

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burger_DB;
+CREATE database burger_DB;
+
+USE burger_DB;
+
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  devour BOOLEAN default false,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devour) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (name, devour) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (name, devour) VALUES ('Western BBQ Burger', false);
