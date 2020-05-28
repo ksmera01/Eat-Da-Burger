f7d5d349eb265e558c200fec231061fcf730d975
Kevin Smeraglio <ksmera01@gmail.com>
2020-05-27 20:01:20 -0400

    updated delete functionality

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -42,23 +42,19 @@ $(function () {
             }
         );
     });
+
+    $(".trashButton").on("click", function (event) {
+        var id = $(this).data("id");
+
+        // Send the DELETE request.
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(
+            function () {
+                console.log("Burger deleted", id);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
 })
-
-
-// $(function () {
-//     $("devourButton").on("click", function (event) {
-//         var id = $(this).data("id");
-
-//         // Send the PUT request.
-//         $.ajax("/api/burgers/" + id, {
-//             type: "PUT",
-//             data: newBurgerState
-//         }).then(
-//             function () {
-//                 console.log("Burger was demolished");
-//                 // Reload the page to get the updated list
-//                 location.reload();
-//             }
-//         );
-//     });
-// });
